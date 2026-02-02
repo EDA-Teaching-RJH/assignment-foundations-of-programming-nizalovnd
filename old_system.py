89c41b11ceda56d1fefebe9a9651ac482023d2f7
nizalovnd <ndn1@kent.ac.uk>
2026-02-02 12:35:05 +0000

    bug 4

--- old_system.py
+++ old_system.py
@@ -60,7 +60,8 @@ def run_system_monolith():
             for rank in r:
                 if rank == "Captain" or "Commander": 
                     count = count + 1
-            print("High ranking officers: " + count) 
+            #str and int cannot be concatenated, converting count to string
+            print("High ranking officers: " + str(count)) 
             
         elif opt == "5":
             print("Shutting down.")
